85ad0843c010af3d2d77dace511b2debe04ab845
Muhammad Idrees <idris@y7mail.com>
2025-04-07 21:49:41 +1000

    feat: update reducer save todos in local storage

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -1,16 +1,28 @@
 import { createReducer, on } from '@ngrx/store';
-import { Todo } from './todo.model';
 import { addTodo, removeTodo, toggleTodo } from './todo.actions';
+import { Todo } from './todo.model';
 
-export const initialState: Todo[] = [];
+// Load todos from localStorage (if available)
+const storedTodos = localStorage.getItem('todos');
+export const initialState: Todo[] = storedTodos ? JSON.parse(storedTodos) : [];
 
 export const todoReducer = createReducer(
   initialState,
-  on(addTodo, (state, { todo }) => [...state, todo]),
-  on(removeTodo, (state, { id }) => state.filter((todo) => todo.id !== id)),
-  on(toggleTodo, (state, { id }) =>
-    state.map((todo) =>
+  on(addTodo, (state, { todo }) => {
+    const updatedState = [...state, todo];
+    localStorage.setItem('todos', JSON.stringify(updatedState)); // Save to localStorage
+    return updatedState;
+  }),
+  on(removeTodo, (state, { id }) => {
+    const updatedState = state.filter((todo) => todo.id !== id);
+    localStorage.setItem('todos', JSON.stringify(updatedState)); // Save to localStorage
+    return updatedState;
+  }),
+  on(toggleTodo, (state, { id }) => {
+    const updatedState = state.map((todo) =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    )
-  )
+    );
+    localStorage.setItem('todos', JSON.stringify(updatedState)); // Save to localStorage
+    return updatedState;
+  })
 );
