973f97274530b4bc7dd273819e345a78548793fe
Muhammad Idrees <idris@y7mail.com>
2025-04-09 19:25:44 +1000

    wip: update reducer state

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -7,27 +7,45 @@ import {
 } from './todo.actions';
 import { Todo } from './todo.model';
 
+export interface TodoState {
+  todos: Todo[];
+  error: string | null;
+}
+
 const storedTodos = localStorage.getItem('todos');
-export const initialState: Todo[] = storedTodos ? JSON.parse(storedTodos) : [];
+// export const initialState: TodoState = storedTodos ? JSON.parse(storedTodos) : [];
+export const initialState: TodoState = {
+  todos: storedTodos ? JSON.parse(storedTodos) : [],
+  error: null,
+};
 
 export const todoReducer = createReducer(
   initialState,
-  on(loadTodosSuccess, (state, { todos }) => [...state, ...todos]),
+  on(loadTodosSuccess, (state, { todos }) => {
+    const updatedState = { ...state, todos };
+    return updatedState;
+  }),
   on(addTodo, (state, { todo }) => {
-    const updatedState = [...state, todo];
-    localStorage.setItem('todos', JSON.stringify(updatedState));
+    const updatedState = { ...state, todos: [...state.todos, todo] };
+    localStorage.setItem('todos', JSON.stringify(updatedState.todos));
     return updatedState;
   }),
   on(removeTodo, (state, { id }) => {
-    const updatedState = state.filter((todo) => todo.id !== id);
-    localStorage.setItem('todos', JSON.stringify(updatedState));
+    const updatedState = {
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    };
+    localStorage.setItem('todos', JSON.stringify(updatedState.todos));
     return updatedState;
   }),
   on(toggleTodo, (state, { id }) => {
-    const updatedState = state.map((todo) =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    );
-    localStorage.setItem('todos', JSON.stringify(updatedState));
+    const updatedState = {
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    };
+    localStorage.setItem('todos', JSON.stringify(updatedState.todos));
     return updatedState;
   })
 );
