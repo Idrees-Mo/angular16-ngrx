fa2a127c8f474f63fed3efda37b8b634cb54a366
Muhammad Idrees <idris@y7mail.com>
2025-04-12 13:54:53 +1000

    feat: basic routes for todos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { todosRoutes } from './todos/todos.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todos', children: todosRoutes },
+  {
+    path: '',
+    redirectTo: 'todos',
+    pathMatch: 'full',
+  },
+];
