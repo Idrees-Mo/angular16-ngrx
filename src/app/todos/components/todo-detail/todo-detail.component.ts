43f1cc5f39d8edd87fa6267a7a37577ea06ab966
Muhammad Idrees <idris@y7mail.com>
2025-04-12 21:04:36 +1000

    wip: use router selector to get todoId from param

--- src/app/todos/components/todo-detail/todo-detail.component.ts
+++ src/app/todos/components/todo-detail/todo-detail.component.ts
@@ -1,12 +1,31 @@
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, combineLatest, filter, map } from 'rxjs';
+import { selectAllTodos, selectTodoIdParam } from '../../state/todo.selectors';
+import { Todo } from '../../models/todo.model';
+import {
+  selectRouteParam,
+  selectRouter,
+} from '../../../shared/router/router.selector';
 
 @Component({
   selector: 'app-todo-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './todo-detail.component.html',
-  styleUrl: './todo-detail.component.scss'
+  styleUrl: './todo-detail.component.scss',
 })
 export class TodoDetailComponent {
+  todoId$: string | unknown;
+  todo$: Observable<Todo | undefined>;
 
+  constructor(private store: Store) {
+    this.todo$ = combineLatest([
+      this.store.select(selectAllTodos),
+      this.store.select(selectTodoIdParam),
+    ]).pipe(map(([todos, id]) => todos.find((todo) => todo.id === +id!)));
+
+    this.store.select(selectRouter).subscribe(console.warn);
+  }
 }
