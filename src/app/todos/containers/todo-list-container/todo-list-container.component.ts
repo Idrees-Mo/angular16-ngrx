b4f366d3b4e34056bd6a5844696e557255682000
Muhammad Idrees <idris@y7mail.com>
2025-04-12 13:15:05 +1000

    feat: add basic todo components

---
+++ src/app/todos/containers/todo-list-container/todo-list-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-list-container.component.html',
+  styleUrl: './todo-list-container.component.scss'
+})
+export class TodoListContainerComponent {
+
+}
