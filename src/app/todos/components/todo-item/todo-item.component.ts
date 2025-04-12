648521a01171dff0a69295efe366b4d651415e8f
Muhammad Idrees <idris@y7mail.com>
2025-04-12 13:55:41 +1000

    wip: todoItem component

--- src/app/todos/components/todo-item/todo-item.component.ts
+++ src/app/todos/components/todo-item/todo-item.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
   imports: [],
   templateUrl: './todo-item.component.html',
-  styleUrl: './todo-item.component.scss'
+  styleUrl: './todo-item.component.scss',
 })
 export class TodoItemComponent {
-
+  @Input() todo!: Todo;
+  @Output() view = new EventEmitter<number>();
 }
